b282189bf8d30d8057b42da6f39697f637d62f18
Rajat Karahe <29karaherajat@gmail.com>
2020-05-31 02:46:53 +0530

    app complete

--- blogApp/blog/models.py
+++ blogApp/blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -10,4 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-    	return self.title
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
